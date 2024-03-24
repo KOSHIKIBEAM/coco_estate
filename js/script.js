dda1fce251b78bea33353b4a76aacf57db040f6a
keita <dmwwvn@gmail.com>
2024-03-25 00:46:25 +0900

    ⚡️ PC-Q%A完了

--- js/script.js
+++ js/script.js
@@ -5,24 +5,6 @@ $('a[href*="#"]').click(function () {
   return false;
 });
 
-$(function () {
-  // ハンバーガーボタンクリックで実行
-  $("#menu-icon").click(function () {
-    $(this).toggleClass("active");
-    $("#menu-panel").toggleClass("active");
-    $("body").toggleClass("active");
-    $("p-drawer__bg").toggleClass("active");
-  });
-
-  $(".p-drawer__panel-list li a").click(function () {
-    $("#menu-icon").removeClass("active");
-    $("#menu-panel").removeClass("active");
-    $("body").removeClass("active");
-    $("p-drawer__bg").removeClass("active");
-  });
-  // function
-});
-
 const intersectionObserver = new IntersectionObserver(function (entries) {
   entries.forEach(function (entry) {
     if (entry.isIntersecting) {
