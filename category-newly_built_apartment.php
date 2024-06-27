6fb4dd0ee730cba5b801a6b5b06bfad0314a3957
KOSHIKIBEAM <142467321+KOSHIKIBEAM@users.noreply.github.com>
2024-06-27 19:30:29 +0900

    修正

--- category-newly_built_apartment.php
+++ category-newly_built_apartment.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <?php get_template_part('template-parts/fv'); ?>
 
-<section class="l-search" id="search">
+<section class="l-search" id="searchSection">
     <div class="p-search">
         <div class="c-section__ttl">
             <h2>SEARCH</h2>
@@ -11,17 +11,17 @@
             <div class="p-search__btn-buy">
                 <p>買う</p>
                 <div class="p-search__btn-buy-btn">
-                    <a href="/category/land">土地</a>
-                    <a href="/category/new_house">新築戸建</a>
-                    <a href="/category/existing_home">中古戸建</a>
-                    <a class="active" href="/category/newly_built_apartment">新築マンション</a>
-                    <a href="/category/used_apartment">中古マンション</a>
+                    <a href="/category/land#searchSection">土地</a>
+                    <a href="/category/new_house#searchSection">新築戸建</a>
+                    <a href="/category/existing_home#searchSection">中古戸建</a>
+                    <a class="active" href="/category/newly_built_apartment#searchSection">新築マンション</a>
+                    <a href="/category/used_apartment#searchSection">中古マンション</a>
                 </div>
             </div>
             <div class="p-search__btn-borrow">
                 <p>借りる</p>
                 <div class="p-search__btn-borrow-btn">
-                    <a href="/category/rental">賃貸<span>(マンション・アパート・一戸建て)</span></a>
+                    <a href="/category/rental#searchSection">賃貸<span>(マンション・アパート・一戸建て)</span></a>
                 </div>
             </div>
         </div>
